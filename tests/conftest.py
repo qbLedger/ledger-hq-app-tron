da372dff98d0af24fb7c98e196433a6d2e512983
Alexis Grojean <alexis.grojean@ledger.fr>
2023-03-06 15:41:39 +0100

    PR review.

--- tests/conftest.py
+++ tests/conftest.py
@@ -12,38 +12,49 @@ MNEMONIC = "abandon abandon abandon abandon abandon abandon abandon abandon aban
 configuration.OPTIONAL.BACKEND_SCOPE = "session"
 configuration.OPTIONAL.CUSTOM_SEED = MNEMONIC
 
+
 @pytest.fixture(scope="session")
 def configuration(backend: BackendInterface, firmware):
     if type(backend) is SpeculosBackend:
         if firmware.device == "stax":
             # Go to settings menu.
-            backend.finger_touch(x=300,y=30,delay=0.5)
+            backend.finger_touch(x=300, y=30, delay=0.5)
             # Allow data in TXs
-            backend.finger_touch(x=30,y=100,delay=0.5)
+            backend.finger_touch(x=30, y=100, delay=0.5)
             # Allow custom contracts
-            backend.finger_touch(x=30,y=250,delay=0.5)
+            backend.finger_touch(x=30, y=250, delay=0.5)
             # Allow sign by hash
-            backend.finger_touch(x=30,y=350,delay=0.5)
+            backend.finger_touch(x=30, y=350, delay=0.5)
             # Go back to main menu.
-            backend.finger_touch(x=40,y=40,delay=0.5)
+            backend.finger_touch(x=40, y=40, delay=0.5)
             sleep(4)
         else:
             # Go to settings main menu
             backend.right_click()
+            sleep(0.1)
             backend.right_click()
-            backend.both_click()                
+            sleep(0.1)
+            backend.both_click()
+            sleep(0.1)
             # Allow data in TXs
             backend.both_click()
+            sleep(0.1)
             # Allow custom contracts
             backend.right_click()
+            sleep(0.1)
             backend.both_click()
             # Allow sign by hash
             backend.right_click()
+            sleep(0.1)
             backend.right_click()
+            sleep(0.1)
             backend.both_click()
+            sleep(0.1)
             # Go back to main menu
             backend.right_click()
+            sleep(0.1)
             backend.both_click()
+
 
 #########################
 ### CONFIGURATION END ###
